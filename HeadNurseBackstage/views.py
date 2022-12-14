9b19e6704aa8ec848442c0fcba9581aea14566e9
Leo T <79355500+randytaylor4869@users.noreply.github.com>
2022-12-14 15:38:51 +0800

    完成隐患报告的前后端交互
    完成隐患报告的前后端交互

--- HeadNurseBackstage/views.py
+++ HeadNurseBackstage/views.py
@@ -9,20 +9,21 @@ import os
 from django.utils import timezone
 import pytz
 import time
+from urllib import parse
 # Create your views here.
 
-nursedata = {"123456":{"NurseName":"丁真","NurseWorkPermitNumber":"123456","NurseWorkPermitPassword":"666",
-                       "usrname":"瑞克五代","password":"测尼玛"},
-             "1":{"NurseName":"王源","NurseWorkPermitNumber":"2","NurseWorkPermitPassword":"3",
-                       "usrname":"5","password":"6"}}
-inspectiondata = {"123456":{"idNurse":"123456","location":"四川理塘","inspectionInfo":"妈妈在给我充电子烟",
-                       "inspectionDateTime":"2022/11/19 11:00"},
-                  "1":{"idNurse":"123456","location":"四川理塘","inspectionInfo":"妈妈在给我充电子烟",
-                       "inspectionDateTime":"2022/11/19 11:00"}}
-hazarddata = {"123456":{"idNurse":"123456","location":"四川理塘","hazardInfo":"雪豹闭嘴",
-                       "hazardDateTime":"2022/11/19 11:00"},
-              "1":{"idNurse":"123456","location":"四川理塘","hazardInfo":"雪豹闭嘴",
-                       "hazardDateTime":"2022/11/19 11:00"}}
+# nursedata = {"123456":{"NurseName":"丁真","NurseworkPermitNumber":"123456","NurseWorkPermitPassword":"666",
+#                        "usrname":"瑞克五代","password":"测尼玛"},
+#              "1":{"NurseName":"王源","NurseworkPermitNumber":"2","NurseWorkPermitPassword":"3",
+#                        "usrname":"5","password":"6"}}
+# inspectiondata = {"123456":{"idNurse":"123456","location":"四川理塘","inspectionInfo":"妈妈在给我充电子烟",
+#                        "inspectionDateTime":"2022/11/19 11:00"},
+#                   "1":{"idNurse":"123456","location":"四川理塘","inspectionInfo":"妈妈在给我充电子烟",
+#                        "inspectionDateTime":"2022/11/19 11:00"}}
+# hazarddata = {"123456":{"idNurse":"123456","location":"四川理塘","hazardInfo":"雪豹闭嘴",
+#                        "hazardDateTime":"2022/11/19 11:00"},
+#               "1":{"idNurse":"123456","location":"四川理塘","hazardInfo":"雪豹闭嘴",
+#                        "hazardDateTime":"2022/11/19 11:00"}}
 
 def logpage(request):
     if request.method == "GET":
@@ -65,7 +66,7 @@ def registerpage(request):
         #存储用户名密码
         register_info= decode_jwt(storejwt)
         print(register_info)
-        workPermitNumber = register_info['NurseWorkPermitNumber']
+        workPermitNumber = register_info['NurseworkPermitNumber']
         workPermitPassword = register_info['NurseWorkPermitPassword']
         username = register_info['username']
         password = register_info['password']
@@ -101,7 +102,7 @@ def mainpage(request):
             username = username+","
         except:
             username = ""        
-            timenow = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        timenow = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
         return render(request,"mainpage.html",{"username":username,"time":timenow})
     # elif request.method == "POST":
     #     return redirect("/")
@@ -113,8 +114,8 @@ def nursemanagementpage(request):
         for obj in objs:
             nursedata[str(obj.id)] = {
                 "NurseName":obj.name,
-                "NurseWorkPermitNumber": obj.workPermitNumber,
-                "NurseWorkPermitPassword":obj.workPermitPassword,
+                "NurseworkPermitNumber": obj.workPermitNumber,
+                "NurseworkPermitPassword":obj.workPermitPassword,
                 "username":obj.username,
                 "password":obj.password
             }
@@ -135,6 +136,7 @@ def nursemanagementpage(request):
         workPermitNumber=listtext['workPermitNumber'],workPermitPassword=listtext['workPermitPassword'],login_jwt=loginjwt)
         if nurse:
             returnjson = {'state':'200','data':[]}
+            print(returnjson)
             return HttpResponse(json.dumps(returnjson))
         else:
             returnjson = {'state':'400','message':'nurse create fail'}
@@ -143,8 +145,8 @@ def nursemanagementpage(request):
             return HttpResponse(json.dumps(returnjson))
     elif request.method == "PUT":
         qstr = request.META['QUERY_STRING']
-        workPermitNumber = qstr[qstr.find('=')+1:]
-        print('nursemanage:',workPermitNumber)
+        workPermitNumber = parse.unquote(qstr[qstr.find('=')+1:])
+        print('nursemanagePUT:',workPermitNumber)
         nurse = NurseInfo.objects.filter(workPermitNumber=workPermitNumber)
         if nurse:
             listtext = json.loads(request.body)
@@ -162,8 +164,8 @@ def nursemanagementpage(request):
             return HttpResponse(json.dumps(returnjson))
     elif request.method == "DELETE":
         qstr = request.META['QUERY_STRING']
-        workPermitNumber = qstr[qstr.find('=')+1:]
-        print('nursemanage:',workPermitNumber)
+        workPermitNumber = parse.unquote(qstr[qstr.find('=')+1:])
+        print('nursemanageDELETE:',workPermitNumber)
         nurse = NurseInfo.objects.filter(workPermitNumber=workPermitNumber)
         if nurse:
             nurse.delete()
@@ -186,11 +188,60 @@ def inspectionpage(request):
                 "inspectionInfo":obj.inspectionInfo,
                 "inspectionDateTime":obj.inspectionDateTime
         }
-        return render(request,"inspectionpage.html",{"inspectiondata":inspectiondata})
+        try:
+            username = request.get_full_path().split("?")[1].split("=")[1]
+            username = username+","
+        except:
+            username = ""
+        timenow = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))            
+        return render(request,"inspectionpage.html",{"inspectiondata":inspectiondata,"username":username,"time":timenow})
     elif request.method == "POST":
-        tmp =  request.POST.get("NurseWorkPermitNumber")
-        print(request.POST)
-        return redirect("/inspectionpage")
+        listtext = json.loads(request.body)
+        print(listtext)
+        obj = InspectionReports.objects.create(idNurse=listtext['idNurse'],location=listtext['location'],
+        inspectionInfo=listtext['inspectionInfo'],inspectionDateTime=listtext['inspectionDateTime'])
+        if obj == None :
+            returnjson = {'state':'400','message':'add inspection report failed'}
+            return HttpResponse(json.dumps(returnjson))
+        else:
+            returnjson = {'state':'200','data':[]}
+            return HttpResponse(json.dumps(returnjson))
+    elif request.method == "PUT":
+        print(request.get_full_path())
+        text1 = request.get_full_path().split('?')[1]
+        text2 = text1.split('&')
+        idNurse = text2[0].split('=')[1]
+        dateTime = parse.unquote(text2[1].split('=')[1])
+        inspection = InspectionReports.objects.filter(idNurse=idNurse,inspectionDateTime=dateTime)
+        if inspection:
+            listtext = json.loads(request.body)
+            print(listtext)
+            location = listtext['location']
+            inspectionInfo = listtext['inspectionInfo']
+            inspection.update(location=location,inspectionInfo=inspectionInfo)
+            returnjson = {'state':'200','data':[]}
+            return HttpResponse(json.dumps(returnjson))
+        else:
+            returnjson = {'state':'400','message':'no such inspectionreport'}
+            print('inspectionpage.巡视记录不存在：')
+            print(returnjson)
+            return HttpResponse(json.dumps(returnjson))
+    elif request.method == "DELETE":
+        print(request.get_full_path())
+        text1 = request.get_full_path().split('?')[1]
+        text2 = text1.split('&')
+        idNurse = text2[0].split('=')[1]
+        dateTime = parse.unquote(text2[1].split('=')[1])
+        inspection = InspectionReports.objects.filter(idNurse=idNurse,inspectionDateTime=dateTime)
+        if inspection:
+            inspection.delete()
+            returnjson = {'state':'200','data':[]}
+            return HttpResponse(json.dumps(returnjson))
+        else:
+            returnjson = {'state':'400','message':'no such inspectionreport'}
+            print('inspectionpage.巡视记录不存在：')
+            print(returnjson)
+            return HttpResponse(json.dumps(returnjson))
 
 def hazardpage(request):
     if request.method == "GET":
@@ -203,10 +254,60 @@ def hazardpage(request):
                 "hazardInfo":obj.hazardInfo,
                 "hazardDateTime":obj.hazardDateTime
         }
-        return render(request,"hazardpage.html",{"hazarddata":hazarddata})
+        try:
+            username = request.get_full_path().split("?")[1].split("=")[1]
+            username = username+","
+        except:
+            username = ""
+        timenow = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))            
+        return render(request,"hazardpage.html",{"hazarddata":hazarddata,"username":username,"time":timenow})
     elif request.method == "POST":
-        print(request.POST)
-        return redirect("/hazardpage")
+        listtext = json.loads(request.body)
+        print('hazard.POST',listtext)
+        obj = HazardReports.objects.create(idNurse=listtext['idNurse'],location=listtext['location'],
+        hazardInfo=listtext['hazardInfo'],hazardDateTime=listtext['hazardDateTime'])
+        if obj == None :
+            returnjson = {'state':'400','message':'add hazard report failed'}
+            return HttpResponse(json.dumps(returnjson))
+        else:
+            returnjson = {'state':'200','data':[]}
+            return HttpResponse(json.dumps(returnjson))
+    elif request.method == "PUT":
+        print(request.get_full_path())
+        text1 = request.get_full_path().split('?')[1]
+        text2 = text1.split('&')
+        idNurse = text2[0].split('=')[1]
+        dateTime = parse.unquote(text2[1].split('=')[1])
+        hazard = HazardReports.objects.filter(idNurse=idNurse,hazardDateTime=dateTime)
+        if hazard:
+            listtext = json.loads(request.body)
+            print(listtext)
+            location = listtext['location']
+            hazardInfo = listtext['hazardInfo']
+            hazard.update(location=location,hazardInfo=hazardInfo)
+            returnjson = {'state':'200','data':[]}
+            return HttpResponse(json.dumps(returnjson))
+        else:
+            returnjson = {'state':'400','message':'no such hazardreport'}
+            print('hazardpage.隐患记录不存在：')
+            print(returnjson)
+            return HttpResponse(json.dumps(returnjson))
+    elif request.method == "DELETE":
+        print(request.get_full_path())
+        text1 = request.get_full_path().split('?')[1]
+        text2 = text1.split('&')
+        idNurse = text2[0].split('=')[1]
+        dateTime = parse.unquote(text2[1].split('=')[1])
+        hazard = HazardReports.objects.filter(idNurse=idNurse,hazardDateTime=dateTime)
+        if hazard:
+            hazard.delete()
+            returnjson = {'state':'200','data':[]}
+            return HttpResponse(json.dumps(returnjson))
+        else:
+            returnjson = {'state':'400','message':'no such hazardreport'}
+            print('hazardpage.隐患记录不存在：')
+            print(returnjson)
+            return HttpResponse(json.dumps(returnjson))
 
 
 class nurseGroups(View):
